25fe7fa2812055d6fc3e56c68fa7824c0711d785
Duc An <an.nguyenhoduc@hcmut.edu.vn>
2025-05-08 16:52:08 +0700

    feat: add background image and remove printer image

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,23 +1,27 @@
 import { Box, Container, Typography } from "@mui/material";
 import printer from "../assets/printer.png";
 import { useAuth } from "../contexts/AuthContext";
+import styles from "../styles/Schedule.module.css";
 
 export default function Home() {
   return (
-    <Container
-      sx={{
-        display: "flex",
-        height: "80vh",
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}>
-      <Box sx={{ color: "primary.main" }}>
-        <Typography variant="h1" sx={{ fontWeight: "500" }}>
-          Chào mừng!
-        </Typography>
-        <Typography variant="h3">Đến với trang chủ</Typography>
-      </Box>
-      <img src={printer} height={600} width={400}></img>
-    </Container>
+    <>
+      <div className={styles.pageBackground}></div>
+      <Container
+        sx={{
+          display: "flex",
+          height: "80vh",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box sx={{ color: "primary.main" }}>
+          <Typography variant="h1" sx={{ fontWeight: "500" }}>
+            Chào mừng!
+          </Typography>
+          <Typography variant="h3">Đến với trang chủ</Typography>
+        </Box>
+      </Container>
+    </>
   );
 }
